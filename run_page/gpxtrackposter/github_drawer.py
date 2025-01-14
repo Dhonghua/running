3782858a4531f84f27b707f3c04c0e1993e10602
Honghua <39397680+Dhonghua@users.noreply.github.com>
2025-01-14 10:07:38 +0800

    Update github_drawer.py

--- run_page/gpxtrackposter/github_drawer.py
+++ run_page/gpxtrackposter/github_drawer.py
@@ -62,18 +62,18 @@ class GithubDrawer(TracksDrawer):
             except Exception as e:
                 print(str(e))
                 month_names = [
-                    "一月",
-                    "二月",
-                    "三月",
-                    "四月",
-                    "五月",
-                    "六月",
-                    "七月",
-                    "八月",
-                    "九月",
-                    "十月",
-                    "十一月",
-                    "十二月",
+                    "Jan",
+                    "Feb",
+                    "Mar",
+                    "Apr",
+                    "May",
+                    "Jun",
+                    "Jul",
+                    "Aug",
+                    "Sep",
+                    "Oct",
+                    "Nov",
+                    "Dec",
                 ]
             km_or_mi = "mi"
             if self.poster.units == "metric":
